08c158983474b6e940a31eabbdb7848d0cf66894
Joe Boylson <34290408+joeboylson@users.noreply.github.com>
2024-02-09 16:30:05 +0000

    feat: add privacy policy;

---
+++ src/App/index.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <img src="header-logo.png" alt="PlaceLift Logo" />
+      <iframe title="Privacy Policy" src="privacy-policy.html" />
+    </>
+  );
+}
+
+export default App;
